2a4e48bc9da0a81aae2f211090ea282ce5441c9b
Niakr1s <pavel2188@gmail.com>
2026-09-11 10:43:28 +0300

    added all groups

--- mgr/executable_groups.sh
+++ mgr/executable_groups.sh
@@ -4,29 +4,41 @@
 
 # Define the groups you want to add the user to
 GROUPS_TO_ADD=(
-  input
+    sys
+    network
+    libvirt
+    greeter
+    nopasswdlogin
+    rfkill
+    users
+    video
+    storage
+    lp
+    input
+    audio
+    wheel
 )
 
 # Make sure we're not running as root (optional sanity check)
 if [[ "$(id -u)" -eq 0 ]]; then
-  echo "Warning: running as root. \$USER is 'root'." >&2
+    echo "Warning: running as root. \$USER is 'root'." >&2
 fi
 
 # Verify the script isn't run with sudo directly (which would make $USER=root)
 if [[ -n "${SUDO_USER:-}" ]]; then
-  echo "Warning: you invoked this via sudo. Using SUDO_USER='$SUDO_USER' instead of '$USER'."
-  TARGET_USER="$SUDO_USER"
+    echo "Warning: you invoked this via sudo. Using SUDO_USER='$SUDO_USER' instead of '$USER'."
+    TARGET_USER="$SUDO_USER"
 else
-  TARGET_USER="$USER"
+    TARGET_USER="$USER"
 fi
 
 for group in "${GROUPS_TO_ADD[@]}"; do
-  if getent group "$group" > /dev/null 2>&1; then
-    echo "Adding $TARGET_USER to group: $group"
-    sudo usermod -aG "$group" "$TARGET_USER"
-  else
-    echo "Skipping '$group': group does not exist on this system." >&2
-  fi
+    if getent group "$group" > /dev/null 2>&1; then
+        echo "Adding $TARGET_USER to group: $group"
+        sudo usermod -aG "$group" "$TARGET_USER"
+    else
+        echo "Skipping '$group': group does not exist on this system." >&2
+    fi
 done
 
 echo "Done. Log out and back in (or run 'newgrp <group>') for changes to take effect."
